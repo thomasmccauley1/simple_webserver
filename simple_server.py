3e42c56286283d24a2632c3f3094db3922a2c232
Thomas McCauley <thomasmccauley.37@gmail.com>
2025-09-07 15:35:24 -0400

    Initial commit: simple webserver

---
+++ simple_server.py
@@ -0,0 +1,18 @@
+import http.server
+import socketserver
+
+PORT = 8081
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        print(f"Received: {post_data.decode('utf-8')}")
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Data received")
+
+with socketserver.TCPServer(("0.0.0.0", PORT), Handler) as httpd:
+    print(f"Serving on port {PORT}")
+    httpd.serve_forever()
+
